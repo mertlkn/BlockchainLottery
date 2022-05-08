8e205193b014a589d758995010048b2825563fc9
mertlkn <m558mert@gmail.com>
2022-05-08 16:43:55 +0300

    New features are added.

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,42 +3,10 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "./TurkishLira.sol";
-library Math {
-   function log2(uint x) public returns (uint y){
-   assembly {
-        let arg := x
-        x := sub(x,1)
-        x := or(x, div(x, 0x02))
-        x := or(x, div(x, 0x04))
-        x := or(x, div(x, 0x10))
-        x := or(x, div(x, 0x100))
-        x := or(x, div(x, 0x10000))
-        x := or(x, div(x, 0x100000000))
-        x := or(x, div(x, 0x10000000000000000))
-        x := or(x, div(x, 0x100000000000000000000000000000000))
-        x := add(x, 1)
-        let m := mload(0x40)
-        mstore(m,           0xf8f9cbfae6cc78fbefe7cdc3a1793dfcf4f0e8bbd8cec470b6a28a7a5a3e1efd)
-        mstore(add(m,0x20), 0xf5ecf1b3e9debc68e1d9cfabc5997135bfb7a7a3938b7b606b5b4b3f2f1f0ffe)
-        mstore(add(m,0x40), 0xf6e4ed9ff2d6b458eadcdf97bd91692de2d4da8fd2d0ac50c6ae9a8272523616)
-        mstore(add(m,0x60), 0xc8c0b887b0a8a4489c948c7f847c6125746c645c544c444038302820181008ff)
-        mstore(add(m,0x80), 0xf7cae577eec2a03cf3bad76fb589591debb2dd67e0aa9834bea6925f6a4a2e0e)
-        mstore(add(m,0xa0), 0xe39ed557db96902cd38ed14fad815115c786af479b7e83247363534337271707)
-        mstore(add(m,0xc0), 0xc976c13bb96e881cb166a933a55e490d9d56952b8d4e801485467d2362422606)
-        mstore(add(m,0xe0), 0x753a6d1b65325d0c552a4d1345224105391a310b29122104190a110309020100)
-        mstore(0x40, add(m, 0x100))
-        let magic := 0x818283848586878898a8b8c8d8e8f929395969799a9b9d9e9faaeb6bedeeff
-        let shift := 0x100000000000000000000000000000000000000000000000000000000000000
-        let a := div(mul(x, magic), shift)
-        y := div(mload(add(m,sub(255,a))), shift)
-        y := add(y, mul(256, gt(arg, 0x8000000000000000000000000000000000000000000000000000000000000000)))
-    }  
-}
-}
 contract Lottery is ERC721 {
 
     struct Ticket {
-        uint256 id;
+        uint256 ticketNo;
         address owner;
         bytes32 hash_rnd_number;
         uint rnd_number;
@@ -51,33 +19,42 @@ contract Lottery is ERC721 {
         uint lotteryNo;
         uint firstTicketNo;
         Ticket[] tickets;
-        uint[] winners;
+        Winner[] winners;
+    }
+
+    struct Winner {
+        uint winnerTicketNo;
+        uint prize;
+        bool collected;
+        bool revealed;
+        bool refunded;
     }
 
     mapping(address => uint256) public balances;
-    //mapping(address => mapping(uint => Ticket)) public tickets;
-    mapping(uint=>uint256[]) public winnerIndices;
     TurkishLira _turkishLira;
     uint256 lastTicketId;
     uint256 currentLotteryId;
     uint purchaseDeadline;
     uint revealDeadline;
     bool isRevealPhase=false;
-    //mapping(uint => Ticket[]) public ticketsOfLottery;
     mapping(uint => uint[]) public winnerTicketsNoOfLottery;
     mapping(uint => uint) public amountCollectedOfLottery;
     mapping(uint => Round) public rounds;
     uint initTime;
+    uint purchasePeriod = 2 minutes;
+    uint revealPeriod = 2 minutes;
 
     constructor(TurkishLira turkishLira) ERC721("Lottery","Ltry") {
         _turkishLira = turkishLira;
-        lastTicketId = 0;
+        lastTicketId = 1;
         currentLotteryId = 0;
-        Round memory firstRound;
+        /*Round memory firstRound;
         firstRound.firstTicketNo = lastTicketId;
-        firstRound.lotteryNo = currentLotteryId;
-        purchaseDeadline = block.timestamp + 4 days;
-        revealDeadline = block.timestamp + 7 days;
+        firstRound.lotteryNo = currentLotteryId;*/
+        rounds[0].firstTicketNo = lastTicketId;
+        rounds[0].lotteryNo = currentLotteryId;
+        purchaseDeadline = block.timestamp + purchasePeriod;
+        revealDeadline = purchaseDeadline + revealPeriod;
         amountCollectedOfLottery[currentLotteryId] = 0;
         initTime = block.timestamp;
     }
@@ -92,8 +69,19 @@ contract Lottery is ERC721 {
       _;
     }
 
-    modifier submissionPhase {
+    modifier purchasePhase {
         require(block.timestamp < purchaseDeadline);
+        _;
+    }
+
+    modifier newRound {
+        if(block.timestamp > revealDeadline) {
+            uint lottery_no = getLotteryNo(block.timestamp);
+            determineWinningNumber(currentLotteryId);
+            currentLotteryId = lottery_no;
+            purchaseDeadline = initTime + currentLotteryId * (purchasePeriod + revealPeriod) + purchasePeriod;
+            revealDeadline = purchaseDeadline + revealPeriod;
+        }
         _;
     }
 
@@ -112,21 +100,18 @@ contract Lottery is ERC721 {
         
     }
 
-    function buyTicket(bytes32 hash_rnd_number) public  {
+    function buyTicket(bytes32 hash_rnd_number) public newRound purchasePhase {
         
         
-        lastTicketId += 1;
         Ticket memory newTicket = Ticket(lastTicketId,msg.sender,hash_rnd_number,0,currentLotteryId,false,false);
-        //tickets[msg.sender][newTicket.id] = newTicket;
-        //ticketsOfLottery[currentLotteryId].push(newTicket);
         rounds[currentLotteryId].tickets.push(newTicket);
         _safeMint(msg.sender, lastTicketId);
-        
+        lastTicketId += 1;
         balances[msg.sender] -= 10;
         amountCollectedOfLottery[currentLotteryId] += 10;
     }
 
-    function collectTicketRefund(uint ticket_no) public submissionPhase {
+    function collectTicketRefund(uint ticket_no) public purchasePhase {
         _burn(ticket_no);
         balances[msg.sender] += 5;
         uint firstTicketNo = rounds[currentLotteryId].firstTicketNo;
@@ -134,11 +119,11 @@ contract Lottery is ERC721 {
         amountCollectedOfLottery[currentLotteryId] -= 10; // refund 5 but keep 5 for the contract?
     }
 
-    function revealRndNumber(uint ticketno, uint rnd_number) public {
+    function revealRndNumber(uint ticketno, uint rnd_number) public revealPhase{
         if(ownerOf(ticketno) != msg.sender) {
             revert();
         }
-        bytes32 keccak=keccak256(abi.encodePacked(rnd_number));
+        bytes32 keccak=keccak256(abi.encodePacked(msg.sender,rnd_number));
         uint firstTicketNo = rounds[currentLotteryId].firstTicketNo;
         if(rounds[currentLotteryId].tickets[ticketno-firstTicketNo].hash_rnd_number != keccak) {
             revert();
@@ -148,21 +133,23 @@ contract Lottery is ERC721 {
     }
 
     function getLastOwnedTicketNo(uint lottery_no) public view returns(uint,uint8 status) {
+        lottery_no--;
         uint i = rounds[lottery_no].tickets.length-1;
         for(;i >= 0; i--) {
             if(rounds[lottery_no].tickets[i].owner == msg.sender) {
-                return (rounds[lottery_no].tickets[i].id,1);
+                return (rounds[lottery_no].tickets[i].ticketNo,1);
             }
         }
         return (0,0);
     }
 
     function getIthOwnedTicketNo(uint i,uint lottery_no) public view returns(uint,uint8 status) {
+        lottery_no--;
         uint j = 0;
         for(;j < rounds[lottery_no].tickets.length; j++) {
             if(rounds[lottery_no].tickets[j].owner == msg.sender) {
-                if(i == 0) {
-                    return (rounds[lottery_no].tickets[j].id,1);
+                if(i == 1) {
+                    return (rounds[lottery_no].tickets[j].ticketNo,1);
                 }
                 i--;
             }
@@ -172,43 +159,79 @@ contract Lottery is ERC721 {
 
     // it should be run end of the reveal phase
     function determineWinningNumber(uint lottery_no) public {
+        lottery_no--;
+        Round memory round = rounds[lottery_no];
         uint i = 0;
-        uint length = rounds[lottery_no].tickets.length;
-        while(i < length && !rounds[lottery_no].tickets[i].revealed){
-            i++;
+        uint n = 0;
+        for(; i<round.tickets.length; i++) {
+            if(round.tickets[i].revealed == true && round.tickets[i].refunded == false) {
+                n ^= round.tickets[i].rnd_number;
+            }
         }
-        bytes32 winningNumber = bytes32(rounds[lottery_no].tickets[i].rnd_number);
-        for(; i < length; i++) {
-            winningNumber ^= bytes32(rounds[lottery_no].tickets[i].rnd_number);
-
+        uint winnerCount = log(amountCollectedOfLottery[lottery_no])+1;
+        i = 1;
+        for(;i<winnerCount+1; i++) {
+            n = getKeccak256Hash(n);
+            uint prize = (amountCollectedOfLottery[lottery_no]/(2**(i+1))) + (amountCollectedOfLottery[lottery_no]/(2**i)) % 2;
+            Winner memory winner;
+            winner.winnerTicketNo = n%rounds[lottery_no].tickets.length;
+            winner.prize = prize;
+            winner.collected = false;
+            winner.refunded = rounds[lottery_no].tickets[winner.winnerTicketNo-rounds[lottery_no].firstTicketNo].refunded;
+            winner.revealed = rounds[lottery_no].tickets[winner.winnerTicketNo-rounds[lottery_no].firstTicketNo].revealed;
+            rounds[lottery_no].winners.push(winner);
         }
-        winnerIndices[lottery_no].push(uint(winningNumber)%(i+1));
-        winnerTicketsNoOfLottery[lottery_no].push(rounds[lottery_no].tickets[uint(winningNumber)%(i+1)].id);
-
-        uint numOfPrizes=Math.log2(amountCollectedOfLottery[lottery_no]);
-        for(uint j=1; j<numOfPrizes;j++){
-            winningNumber=keccak256(abi.encodePacked(winningNumber));
-            winnerIndices[lottery_no].push((uint256(winningNumber)%(i+1)));
-            winnerTicketsNoOfLottery[lottery_no].push(rounds[lottery_no].tickets[uint(winningNumber)%(i+1)].id);
-        }
-        
     }
 
-    function getKeccak256Hash(uint256 num) public returns (bytes32) {
-        return keccak256(abi.encodePacked(num));
+    function log(uint m) private pure returns(uint ceiling){
+        uint exponential = 1;
+        uint count = 0;
+        while(true) {
+            if(m < exponential) {
+                return count;
+            }
+            count += 1;
+            exponential *= 2;
+        }
+    }
+
+    function getKeccak256Hash(uint256 num) public pure returns (uint hash) {
+        return uint(keccak256(abi.encodePacked(num)));
     }
     /*function getIthPrize(uint i) public returns (uint){
         return amountCollectedOfLottery[0]/2*(i-1) + amountCollectedOfLottery[0]/2*(i-1)%2;
     }*/
 
-    function checkIfTicketWon(uint ticket_no) public view returns (uint amount){
-        for(uint i=1;i<winnerTicketsNoOfLottery[0].length;i++){
-            if(winnerTicketsNoOfLottery[0][i]==ticket_no){
-                return amountCollectedOfLottery[0]/2*(i-1) + amountCollectedOfLottery[0]/2*(i-1)%2;
+    function checkIfTicketWon(uint ticket_no) public view returns (uint amount) {
+        uint i = 0;
+        for(;i<currentLotteryId; i++) {
+            Round memory round = rounds[i];
+            uint j = 0;
+            for(; j<round.winners.length; j++) {
+                if(round.winners[j].winnerTicketNo == ticket_no && !round.winners[j].refunded && round.winners[j].revealed) {
+                    return round.winners[j].prize;
+                }
             }
+        }
+        return 0;
+    }
+
+    function collectTicketPrize(uint ticket_no) public {
+        uint prizeWon = checkIfTicketWon(ticket_no);
+        if(prizeWon == 0) {
+            revert();
+        }
+        if(_turkishLira.transfer(msg.sender,prizeWon)) {
+            revert();
         }
     }
 
+    function getIthWinningTicket(uint i, uint lottery_no) public view returns (uint ticket_no,uint amount) {
+        lottery_no--;
+        return (rounds[lottery_no].winners[i].winnerTicketNo,rounds[lottery_no].winners[i].prize);
+    }
 
-
+    function getLotteryNo(uint unixtimeinweek) public view returns (uint lottery_no) {
+        return (unixtimeinweek - initTime) / (purchasePeriod + revealPeriod) + 1;
+    }
 }
